4fa7a47ba3470bf01b132406c74be22df2035ce8
Akhil Chauhan <akthehacker11@gmail.com>
2023-08-27 21:55:22 +0530

    final changes

--- server.js
+++ server.js
@@ -1,18 +1,55 @@
 const express = require("express");
 const app = express();
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const Document = require("./models/Document");
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/codebin", {});
 
 app.get("/", (req, res) => {
   const code = `Welcome to CodeBin!
 
 Use the commands in the top right corner
 to create a new file to share with others.`;
-  res.render("code-display", { code });
+  res.render("code-display", { code, language: "plaintext" });
 });
 
 app.get("/new", (req, res) => {
   res.render("new");
 });
 
+app.post("/save", async (req, res) => {
+  const value = req.body.value;
+  try {
+    const document = await Document.create({ value });
+    res.redirect(`/${document.id}`);
+  } catch (e) {
+    res.render("new", { value });
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = await Document.findById(id);
+    res.render("code-display", { code: document.value, id });
+  } catch (e) {
+    console.log(e);
+    res.redirect("/");
+  }
+});
+
+app.get("/:id/duplicate", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = await Document.findById(id);
+    res.render("code-display", { code: document.value, id });
+  } catch (e) {
+    res.redirect(`/${id}`);
+  }
+});
 app.listen(3000);
